348c5d5cf1141460ed290cb3ad40770e31166d8c
Sc0utPsyX <V954730ubelin87>
2022-04-25 20:46:03 +0300

    Добавил дополнительный метод считающий сумму массива даже при условии наличия ошибок.

--- src/main/java/MyArrayDataException.java
+++ src/main/java/MyArrayDataException.java
@@ -1,2 +1,7 @@
-public class MyArrayDataException {
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException(int[] i) {
+        super();
+        System.err.printf("Проблемное значение в массиве, которое невозможно преобразовать " +
+                "находится в ячейке %s %s\n", i[0], i[1]);
+    }
 }
